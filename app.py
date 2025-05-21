bf2a9423ca7c2a9d4869248569cd4955aa0c81b7
Yanjia Sun <yanjiasun@Yanjias-Air.lan>
2025-05-21 11:54:00 -0400

    fixed bug

--- app.py
+++ app.py
@@ -1,54 +1,89 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+from model_predict import predict_water_quality
+from utilities import load_water_data, split_scale_data
 import matplotlib.pyplot as plt
-from predict import predict_wqi
+import pandas as pd
 
+
+# file_rawData = "/Users/yanjiasun/Documents/Yanjia_code/WQI-Delaware-ML-WS/Data/Complete_Data_WQI.xlsx"
+file_rawData = "Complete_Data_WQI.xlsx"
+
+df_water_data = load_water_data(file_rawData)
+print('=== Loaded raw water data successfully!!! ===')
+
+features = ['Sulfate (mg/L)', 'Chloride (mg/L)', 'Sodium (mg/L)', 'Potassium (mg/L)',
+                'Calcium (mg/L)', 'Magnesium (mg/L)', 'Total Dissolved Solids (mg/L)',
+                'Turbidity (NTU)', 'Temperature (deg C)', 'pH',
+                'Dissolved Oxygen (mg/L)', 'Nitrate (mg/L)', 'Fecal Coliform (cfu/100ml)']
+
+
+X_train_scaled, X_test_scaled, y_train, y_test, dates_trian, dates_test = split_scale_data(df_water_data)
+print('=== Splited and scaled data successfully!!! ===')
+print(X_train_scaled.shape, X_test_scaled.shape, y_train.shape, y_test.shape)
+
+
+df_X_test_scaled = pd.DataFrame(X_test_scaled, columns=features)
+print('Shape of df_X_test_scaled: ', df_X_test_scaled.shape)
+
+df_y_test = y_test.to_frame('WQI')
+print('Shape of df_y_test: ', df_y_test.shape)
+
+df_dates_test = dates_test.to_frame('ActivityStartDate')
+print('Shape of df_dates_test: ', df_dates_test.shape)
+
+
+
+# st.title("Water Quality Predictor\n(XGBoost With Lag Features)")
 st.set_page_config(page_title="Water Quality Index Predictor", layout="wide")
 
-st.markdown(
-    """
-    <style>
-    .main {
-        background-image: url("https://images.unsplash.com/photo-1502741126161-b048400d8a0d");
-        background-size: cover;
-        background-attachment: fixed;
-    }
-    .block-container {
-        background-color: rgba(255, 255, 255, 0.8);
-        padding: 2rem;
-        border-radius: 10px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+st.title("🌊 Water Quality Index Predictor 🌊\nDriven by XGBoost With Lag Features")
+
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] {{
+    background-image: url("https://www.americanrivers.org/wp-content/uploads/2018/04/5453470115_facb1d7a45_o.jpg");
+    background-size: cover;
+    background-position: center;
+}}
+[data-testid="stHeader"] {{
+    background-color: rgba(255, 255, 255, 0.8);
+}}
+</style>
+"""
+# background-image: url("https://images.unsplash.com/photo-1507525428034-b723cf961d3e");
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 
 with st.container():
-    st.title("🌊 Water Quality Index Predictor 🌊")
+    # st.title("🌊 Water Quality Index Predictor 🌊\nDriven by XGBoost With Lag Features")
 
-    uploaded_file = st.file_uploader("Upload a CSV file (with 13 features + WQI)", type=["csv"])
+    # uploaded_file = st.file_uploader("Upload a CSV file (with 13 features + WQI)", type=["csv"])
 
-    if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file, parse_dates=['ActivityStartDate'])
+    if df_X_test_scaled is not None:
+        # df = pd.read_csv(uploaded_file, parse_dates=['ActivityStartDate'])
+        df = df_X_test_scaled.copy()
 
         if len(df) < 3:
             st.error("CSV must contain at least 3 rows to extract lag features.")
         else:
             st.subheader("Input Features (Last 3 Days)")
 
-            latest_features = df.iloc[-3:, 1:-1].values.flatten().reshape(1, -1)
+            # latest_features = df.iloc[-3:, 1:-1].values.flatten().reshape(1, -1)
+            latest_features = df.iloc[-1:].values.flatten().reshape(1, -1)
 
             col1, col2 = st.columns(2)
             with col1:
-                for i in range(0, len(df.columns[1:-1])//2):
-                    st.text_input(df.columns[1+i], value=f"{df.iloc[-1, 1+i]:.2f}", disabled=True)
+                for i in range(0, len(df.columns)//2):
+                    # st.text_input(df.columns[1+i], value=f"{df.iloc[-1, 1+i]:.2f}", disabled=True)
+                    st.number_input(df.columns[i], value=df.iloc[-1, i], disabled=False)
             with col2:
-                for i in range(len(df.columns[1:-1])//2, len(df.columns[1:-1])):
-                    st.text_input(df.columns[1+i], value=f"{df.iloc[-1, 1+i]:.2f}", disabled=True)
+                for i in range(len(df.columns)//2, len(df.columns)):
+                    # st.text_input(df.columns[1+i], value=f"{df.iloc[-1, 1+i]:.2f}", disabled=True)
+                    st.number_input(df.columns[i], value=df.iloc[-1, i], disabled=False)
 
             if st.button("🔍 Predict WQI"):
-                prediction = predict_wqi(latest_features)
+                prediction = predict_water_quality(latest_features) 
 
                 if prediction < 25:
                     quality = "🌟 Excellent Water Quality 😊👍"
@@ -64,12 +99,12 @@ with st.container():
                 st.markdown(f"### Predicted WQI: `{prediction:.2f}`")
                 st.markdown(f"## {quality}")
 
-            st.subheader("📈 WQI Trend (Past 10 Days)")
+            # st.subheader("📈 WQI Trend (Past 10 Days)")
 
-            fig, ax = plt.subplots()
-            ax.plot(df['ActivityStartDate'], df['WQI'], color='blue', marker='o')
-            ax.set_xlabel("Date")
-            ax.set_ylabel("WQI")
-            ax.set_title("10-Day WQI Trend")
-            ax.grid(True)
-            st.pyplot(fig)
+            # fig, ax = plt.subplots()
+            # ax.plot(df_dates_test['ActivityStartDate'], df_y_test['WQI'], color='blue', marker='o')
+            # ax.set_xlabel("Date")
+            # ax.set_ylabel("WQI")
+            # ax.set_title("10-Day WQI Trend")
+            # ax.grid(True)
+            # st.pyplot(fig)
